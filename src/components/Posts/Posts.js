a11854f0002a6f93000a8c1b48dbfbe07d2871e7
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2020-10-06 15:30:17 -0700

    posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Post from './Post';
 import './Posts.css';
+import Post from './Post'
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+const [post, setPost] = useState('')
   return (
     <div className='posts-container-wrapper'>
+      setPost(post.map(posts => {
+
+      }))
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
